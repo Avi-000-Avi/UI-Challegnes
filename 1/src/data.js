e50022afc4f0b512ddf8c31c22767144140037ae
Avi-000-Avi <atoppo267@gmail.com>
2021-11-07 01:50:58 +0530

    Removed error

---
+++ 1/src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://landingfoliocom.imgix.net/1631667284901-milanote-landing-page-desktop.png?lossless=true&fit=crop&w=900&crop=top,left&auto=format&q=75",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://landingfoliocom.imgix.net/1623895337034-animaapp-landing-page-desktop.png?lossless=true&fit=crop&w=900&crop=top,left&auto=format&q=75",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://landingfoliocom.imgix.net/1622195391172-at-landing-page-desktop.png?lossless=true&fit=crop&w=900&crop=top,left&auto=format&q=75",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://landingfoliocom.imgix.net/1622195391172-at-landing-page-desktop.png?lossless=true&fit=crop&w=900&crop=top,left&auto=format&q=75",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://landingfoliocom.imgix.net/1606971733622-collaborativefund-landing-page-desktop.png?lossless=true&fit=crop&w=900&crop=top,left&auto=format&q=75",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://landingfoliocom.imgix.net/1602987694652-kiff-landing-page-desktop.png?lossless=true&fit=crop&w=900&crop=top,left&auto=format&q=75",
+      link: "http://lama.dev",
+    },
+  ];
